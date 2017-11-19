9661f148aea3ccf3a5188676cc98be05490d733c
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2017-11-19 14:03:19 +0300

    refactor: SerializedVar now uses Filesystem algebra

--- src/mco/io/state/SerializedVar.scala
+++ src/mco/io/state/SerializedVar.scala
@@ -1,44 +1,36 @@
 package mco.io.state
 
 import scalaz._
-import scalaz.syntax.bind._
 
-import better.files._
 import mco.core.Var
 import mco.data.Path
-import mco.util.Capture
+import mco.io.generic.Filesystem
+import mco.util.syntax.fp._
+import mco.util.serialize
+import Filesystem._
 
-// TODO - use Filesystem, not Capture
-class SerializedVar[F[_]: Apply: Capture, A <: Serializable](
+
+class SerializedVar[F[_]: Applicative: Filesystem, A <: Serializable](
   target: Path,
   underlying: Var[F, A]
 ) extends Var[F, A] {
   override def apply(): F[A] = underlying()
-  override def :=(a: A): F[Unit] = Capture {
-    File(target.asString).writeSerialized(a)
-  } *> { underlying := a }
+  override def :=(a: A): F[Unit] = {
+    setBytes(target, serialize.to(a)) *>
+      { underlying := a }
+  }
 }
 
 object SerializedVar {
-  def apply[F[_]: Capture: Bind, A <: Serializable](
+  def apply[F[_]: Monad: Filesystem, A <: Serializable](
     target: Path,
     initial: F[A],
     underlying: A => Var[F, A]
   ): F[Var[F, A]] = {
-    Capture {
-      val file = File(target.asString)
-      if (file.exists) Capture { file.readDeserialized[A] }
-      else initial
-    }
-      .join
-      .map(underlying)
-      .>>! { var0 =>
-        for {
-          value <- var0()
-          _ <- Capture { File(target.asString).writeSerialized(value) }
-        } yield ()
-      }
-      .map(new SerializedVar(target, _))
-      .widen
+    def read = getBytes(target).map(serialize.from[A])
+    for {
+      a <- exists(target).ifM(read, initial)
+      _ <- setBytes(target, serialize.to(a))
+    } yield new SerializedVar(target, underlying(a)): Var[F, A]
   }
 }
