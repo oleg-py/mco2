3599996e0fbb98c8a3f1f502abda5ec0c58ef6a8
Oleg Pyzhcov <oleg.pyzhcov@gmail.com>
2017-12-24 10:38:59 +0300

    chore: add few doc comments

--- src/mco/core/ContentKind.scala
+++ src/mco/core/ContentKind.scala
@@ -3,6 +3,9 @@ package mco.core
 import enumeratum._
 
 
+/**
+ * Enum of roles content inside mod can be treated as
+ */
 sealed trait ContentKind extends EnumEntry
 
 object ContentKind extends Enum[ContentKind] {
--- src/mco/core/Mods.scala
+++ src/mco/core/Mods.scala
@@ -5,12 +5,39 @@ import mco.core.paths._
 import mco.core.state.{Deltas, ModState, RepoState}
 
 
+/**
+ * Algebra of operations over a set of mods
+ * @tparam F - the effect type
+ */
 @forwarders trait Mods[F[_]] {
+  /**
+   * Read current state of repository
+   */
   def state: F[RepoState]
+
+  /**
+   * Update mod inside a repository with provided data
+   * @param key the identifier of the mod
+   * @param diff the changes to be applied to the mod
+   */
   def update(key: RelPath, diff: Deltas.OfMod): F[Unit]
 
+  /**
+   * Remove mod from this repository
+   * @param key the identifier for the mod
+   */
   def remove(key: RelPath): F[Unit]
-  def liftFile(p: Path): F[Option[ModState]]
+
+  /**
+   * Attempt to add provided file to the repository
+   * if it can be treated as a a mod
+   *
+   * @param path the file location
+   * @return an option that is nonempty if there
+   *         was no error lifting the file
+   */
+  def liftFile(path: Path): F[Option[ModState]]
+
   /* TODO
   def setOrder(key: Key, order: Int): F[Unit]
   */
